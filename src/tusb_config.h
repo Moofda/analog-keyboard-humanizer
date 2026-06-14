ae67e93d3e08135ea45da0461bc84117f7032f19
Moofda <mikejshawn@protonmail.com>
2026-06-14 15:23:05 -0700

    Update tusb_config.h

--- src/tusb_config.h
+++ src/tusb_config.h
@@ -5,38 +5,37 @@
 extern "C" {
 #endif
 
-// RHPort number used for device
 #define BOARD_TUD_RHPORT      0
 #define BOARD_TUH_RHPORT      1
 
-// Common config
 #define CFG_TUSB_MCU          OPT_MCU_RP2040
 #define CFG_TUSB_OS           OPT_OS_PICO
 #define CFG_TUSB_DEBUG        0
 
-// Device config
+// Device - XInput output to PC
 #define CFG_TUD_ENABLED       1
-#define CFG_TUD_MAX_SPEED     OPT_MODE_DEFAULT_SPEED
+#define CFG_TUD_MAX_SPEED     OPT_MODE_FULL_SPEED
 #define CFG_TUD_ENDPOINT0_SIZE 64
 
-// Device classes
+// Disable all device classes except vendor
 #define CFG_TUD_HID           0
 #define CFG_TUD_CDC           0
 #define CFG_TUD_MSC           0
 #define CFG_TUD_MIDI          0
+#define CFG_TUD_AUDIO         0
+#define CFG_TUD_VIDEO         0
+#define CFG_TUD_DFU_RUNTIME   0
 #define CFG_TUD_VENDOR        1
+#define CFG_TUD_VENDOR_RX_BUFSIZE 64
+#define CFG_TUD_VENDOR_TX_BUFSIZE 64
 
-// Host config
+// Host - XInput keyboard
 #define CFG_TUH_ENABLED       1
 #define CFG_TUH_RPI_PIO_USB   1
 #define CFG_TUH_MAX_SPEED     OPT_MODE_DEFAULT_SPEED
-
-// XInput host
 #define CFG_TUH_XINPUT        1
 #define CFG_TUH_DEVICE_MAX    1
 #define CFG_TUH_ENUMERATION_BUFSIZE 256
-
-// Host classes
 #define CFG_TUH_HID           0
 #define CFG_TUH_CDC           0
 #define CFG_TUH_MSC           0
