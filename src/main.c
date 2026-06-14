b9fcaeb004a85652291fa6147873d428bd986e1a
Moofda <mikejshawn@protonmail.com>
2026-06-14 14:14:39 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -11,44 +11,19 @@
 #include "humanizer.h"
 
 // Adafruit Feather RP2040 USB Host pins
-#define USB_HOST_DP_PIN  16
-#define USB_HOST_DM_PIN  17
 #define USB_HOST_PWR_PIN 18
 
-// XInput report structure
-typedef struct __attribute__((packed)) {
-    uint8_t  report_id;
-    uint8_t  report_size;
-    uint8_t  buttons1;
-    uint8_t  buttons2;
-    uint8_t  trigger_l;
-    uint8_t  trigger_r;
-    int16_t  stick_lx;
-    int16_t  stick_ly;
-    int16_t  stick_rx;
-    int16_t  stick_ry;
-    uint8_t  reserved[6];
-} XInputReport;
-
-// XInput output report
-typedef struct __attribute__((packed)) {
-    uint8_t report_id;
-    uint8_t report_size;
-    uint8_t rumble_l;
-    uint8_t rumble_r;
-    uint8_t reserved[3];
-} XInputOutReport;
-
-static Humanizer humanizer;
-static XInputReport current_report = {0};
+// XInput report to send to PC
+static uint8_t current_report[20] = {0};
 static bool report_ready = false;
 
+static Humanizer humanizer;
+
 //--------------------------------------------------------------------
-// Core 1 - USB Host (reads keyboard)
+// Core 1 - USB Host
 //--------------------------------------------------------------------
 void core1_main(void)
 {
-    // Enable USB host power
     gpio_init(USB_HOST_PWR_PIN);
     gpio_set_dir(USB_HOST_PWR_PIN, GPIO_OUT);
     gpio_put(USB_HOST_PWR_PIN, 1);
@@ -61,66 +36,52 @@ void core1_main(void)
 }
 
 //--------------------------------------------------------------------
-// XInput host callbacks
+// XInput host callbacks - use correct tusb_xinput signatures
 //--------------------------------------------------------------------
 void tuh_xinput_report_received_cb(uint8_t dev_addr, uint8_t instance,
-                                    xinput_gamepad_t const* report,
+                                    xinputh_interface_t const* xid_itf,
                                     uint16_t len)
 {
     (void)dev_addr; (void)instance; (void)len;
 
-    current_report.report_id  = 0x00;
-    current_report.report_size = 0x14;
-    current_report.buttons1   = (report->wButtons) & 0xFF;
-    current_report.buttons2   = (report->wButtons >> 8) & 0xFF;
-    current_report.trigger_l  = report->bLeftTrigger;
-    current_report.trigger_r  = report->bRightTrigger;
-    current_report.stick_lx   = report->sThumbLX;
-    current_report.stick_ly   = report->sThumbLY;
-    current_report.stick_rx   = report->sThumbRX;
-    current_report.stick_ry   = report->sThumbRY;
+    const xinput_gamepad_t* p = &xid_itf->pad;
 
-    // Apply humanization
-    humanizer_process(&humanizer,
-        &current_report.stick_lx, &current_report.stick_ly,
-        &current_report.stick_rx, &current_report.stick_ry);
+    int16_t lx = p->sThumbLX;
+    int16_t ly = p->sThumbLY;
+    int16_t rx = p->sThumbRX;
+    int16_t ry = p->sThumbRY;
+
+    humanizer_process(&humanizer, &lx, &ly, &rx, &ry);
+
+    current_report[0]  = 0x00;
+    current_report[1]  = 0x14;
+    current_report[2]  = (p->wButtons) & 0xFF;
+    current_report[3]  = (p->wButtons >> 8) & 0xFF;
+    current_report[4]  = p->bLeftTrigger;
+    current_report[5]  = p->bRightTrigger;
+    current_report[6]  = lx & 0xFF;
+    current_report[7]  = (lx >> 8) & 0xFF;
+    current_report[8]  = ly & 0xFF;
+    current_report[9]  = (ly >> 8) & 0xFF;
+    current_report[10] = rx & 0xFF;
+    current_report[11] = (rx >> 8) & 0xFF;
+    current_report[12] = ry & 0xFF;
+    current_report[13] = (ry >> 8) & 0xFF;
 
     report_ready = true;
 }
 
 void tuh_xinput_mount_cb(uint8_t dev_addr, uint8_t instance,
-                          const xinput_gamepad_t* pad)
+                          const xinputh_interface_t* xinput_itf)
 {
-    (void)dev_addr; (void)instance; (void)pad;
+    (void)dev_addr; (void)instance; (void)xinput_itf;
 }
 
 void tuh_xinput_umount_cb(uint8_t dev_addr, uint8_t instance)
 {
     (void)dev_addr; (void)instance;
     report_ready = false;
-}
-
-//--------------------------------------------------------------------
-// TinyUSB device callbacks (XInput output to PC)
-//--------------------------------------------------------------------
-uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id,
-                                 hid_report_type_t report_type,
-                                 uint8_t* buffer, uint16_t reqlen)
-{
-    (void)instance; (void)report_id; (void)report_type;
-    if (reqlen >= sizeof(XInputReport)) {
-        memcpy(buffer, &current_report, sizeof(XInputReport));
-        return sizeof(XInputReport);
-    }
-    return 0;
-}
-
-void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id,
-                             hid_report_type_t report_type,
-                             uint8_t const* buffer, uint16_t bufsize)
-{
-    (void)instance; (void)report_id; (void)report_type;
-    (void)buffer; (void)bufsize;
+    memset(current_report, 0, sizeof(current_report));
 }
 
 //--------------------------------------------------------------------
@@ -130,21 +91,17 @@ int main(void)
 {
     stdio_init_all();
 
-    // Initialize humanizer
     humanizer_init(&humanizer);
 
-    // Initialize USB device (XInput output to PC)
     tud_init(BOARD_TUD_RHPORT);
 
-    // Launch USB host on core 1
     multicore_launch_core1(core1_main);
 
     while (true) {
         tud_task();
 
-        // Send report to PC when ready
         if (report_ready && tud_vendor_mounted()) {
-            tud_vendor_write(&current_report, sizeof(XInputReport));
+            tud_vendor_write(current_report, sizeof(current_report));
             tud_vendor_flush();
             report_ready = false;
         }
