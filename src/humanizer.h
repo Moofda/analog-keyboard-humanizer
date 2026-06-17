ab1dc47d9dafde1b1b2e20860d7a4c559a5cce01
Moofda <mikejshawn@protonmail.com>
2026-06-16 19:33:13 -0700

    Update humanizer.h

--- src/humanizer.h
+++ src/humanizer.h
@@ -3,22 +3,14 @@
 
 #include <stdint.h>
 
+// We will add memory variables here in later phases!
 typedef struct {
-    uint32_t rng_state;
-    
-    // Persistent angle offset memories to enable slow, smooth frame-to-frame drifting
-    float wander_angle_l;
-    float wander_angle_r;
-    
-    // Imperfect centering states
-    int16_t drift_lx;
-    int16_t drift_ly;
-    int16_t drift_rx;
-    int16_t drift_ry;
-    uint32_t last_drift_time;
+    int dummy_init; // Keeps strict C compilers happy for now
 } Humanizer;
 
 void humanizer_init(Humanizer* h);
-void humanizer_process(Humanizer* h, int16_t* lx, int16_t* ly, int16_t* rx, int16_t* ry, uint16_t angle_spread, uint16_t deflection_scale, uint16_t deadzone);
+
+void humanizer_process(Humanizer* h, int16_t* lx, int16_t* ly, int16_t* rx, int16_t* ry, 
+                       uint16_t circ_error);
 
 #endif
