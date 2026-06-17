3a94d50c15beb41fa09349ea33aa20ecb166aaab
Moofda <mikejshawn@protonmail.com>
2026-06-16 19:55:20 -0700

    Update humanizer.h

--- src/humanizer.h
+++ src/humanizer.h
@@ -3,14 +3,14 @@
 
 #include <stdint.h>
 
-// We will add memory variables here in later phases!
 typedef struct {
-    int dummy_init; // Keeps strict C compilers happy for now
+    float noise_l; // Tracks the current wobble angle for the left stick
+    float noise_r; // Tracks the current wobble angle for the right stick
 } Humanizer;
 
 void humanizer_init(Humanizer* h);
 
 void humanizer_process(Humanizer* h, int16_t* lx, int16_t* ly, int16_t* rx, int16_t* ry, 
-                       uint16_t circ_error);
+                       uint16_t circ_error, uint16_t axis_deviation);
 
 #endif
