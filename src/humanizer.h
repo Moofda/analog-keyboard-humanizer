5308ee843a003b5d86e4a41b86dcff6b3d9eeec0
Moofda <mikejshawn@protonmail.com>
2026-06-17 18:35:27 -0700

    Update humanizer.h

--- src/humanizer.h
+++ src/humanizer.h
@@ -4,16 +4,19 @@
 #include <stdint.h>
 
 typedef struct {
-    float phase_l;    // The continuous "time" clock for the left stick wave
-    float prev_mag_l; // Tracks velocity for the release turbo
+    float phase_l;    
+    float prev_mag_l; 
+    float prev_out_x_l; // PHASE 3 MEMORY: Left X
+    float prev_out_y_l; // PHASE 3 MEMORY: Left Y
     
-    float phase_r;    // The continuous "time" clock for the right stick wave
+    float phase_r;    
     float prev_mag_r; 
+    float prev_out_x_r; // PHASE 3 MEMORY: Right X
+    float prev_out_y_r; // PHASE 3 MEMORY: Right Y
 } Humanizer;
 
 void humanizer_init(Humanizer* h);
 
-// UPDATED: Added smoothing_rate to the header!
 void humanizer_process(Humanizer* h, int16_t* lx, int16_t* ly, int16_t* rx, int16_t* ry, 
                        uint16_t circ_error, uint16_t axis_deviation, uint16_t smoothing_rate);
 
