3609c07f2c2163e8454a781fe5f93f0707d84c84
Ramboss <mikejshawn@protonmail.com>
2026-06-22 08:48:04 -0700

    Update humanizer.h

--- src/humanizer.h
+++ src/humanizer.h
@@ -3,35 +3,32 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-#include <math.h>
 
-// Struct to hold stateful memory for the physics engine
 typedef struct {
-    // Oscillators for organic breathing
     float wobble_phase;
     float tilt_phase;
     float gate_phase;
     
-    // EMA (Exponential Moving Average) Smoothing state
-    float ema_lx, ema_ly;
-    float ema_rx, ema_ry;
-
-    // Landing Hysteresis (Dice Roll) state
+    // 2nd-Order Physics State Variables
+    float pos_lx, pos_ly;
+    float vel_lx, vel_ly;
+    float pos_rx, pos_ry;
+    float vel_rx, vel_ry;
+    
     bool was_active_l;
     float land_offset_l;
-    
     bool was_active_r;
     float land_offset_r;
 } Humanizer;
 
-// Initialize the engine memory
 void humanizer_init(Humanizer* h);
 
-// The core physics loop
 void humanizer_process(Humanizer* h, int16_t* lx, int16_t* ly, int16_t* rx, int16_t* ry,
                        uint16_t circ_error, 
                        uint16_t jitter_mag, uint16_t jitter_inner, uint16_t jitter_outer, 
                        uint16_t smoothing_rate, uint16_t gate_level,
-                       uint16_t tilt_deg, uint16_t landing_var, uint16_t passthrough);
+                       uint16_t tilt_deg, uint16_t landing_var, 
+                       uint16_t diagonal_feel, // <-- The new dial
+                       uint16_t passthrough);
 
 #endif // HUMANIZER_H
