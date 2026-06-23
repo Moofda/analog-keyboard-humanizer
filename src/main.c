691b6d3056e3617a113cf4acdaeece3571989ec6
Ramboss <mikejshawn@protonmail.com>
2026-06-22 19:09:04 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -55,6 +55,8 @@ static uint32_t combo_start_time = 0;
 static bool pending_save_and_reboot = false;
 static uint32_t save_trigger_time = 0;
 static uint32_t last_math_tick_us = 0; 
+static int16_t preview_lx = 0, preview_ly = 0;
+static uint32_t last_preview_ms = 0;
 
 void load_settings_from_flash(void) {
     humanizer_config_t *flash_profile = (humanizer_config_t *) flash_target_contents;
@@ -139,22 +141,23 @@ void core1_main(void) {
     pio_cfg.pin_dp = 16; 
     tuh_configure(BOARD_TUH_RHPORT, TUH_CFGID_RPI_PIO_USB_CONFIGURATION, &pio_cfg);
     tuh_init(BOARD_TUH_RHPORT);
-    while (true) {
+ while (true) {
         tuh_task();
-        uint32_t now = to_ms_since_boot(get_absolute_time());
-        uint32_t ints = save_and_disable_interrupts();
-        uint16_t btns = latest_buttons;
-        restore_interrupts(ints);
-        if ((btns & 0x0310) == 0x0310) {
-            if (combo_start_time == 0) { combo_start_time = now; } 
-            else if (now - combo_start_time >= 3000) {
-                watchdog_hw->scratch[0] = CONFIG_MAGIC_NUM;
-                watchdog_reboot(0, 0, 10);
-                while(1);
-            }
-        } else { combo_start_time = 0; }
+        if (!tud_in_config_mode()) {
+            uint32_t now = to_ms_since_boot(get_absolute_time());
+            uint32_t ints = save_and_disable_interrupts();
+            uint16_t btns = latest_buttons;
+            restore_interrupts(ints);
+            if ((btns & 0x0310) == 0x0310) {
+                if (combo_start_time == 0) { combo_start_time = now; }
+                else if (now - combo_start_time >= 3000) {
+                    watchdog_hw->scratch[0] = CONFIG_MAGIC_NUM;
+                    watchdog_reboot(0, 0, 10);
+                    while(1);
+                }
+            } else { combo_start_time = 0; }
+        }
     }
-}
 
 void tuh_xinput_report_received_cb(uint8_t dev_addr, uint8_t instance, xinputh_interface_t const* xid_itf, uint16_t len) {
     (void)dev_addr; (void)instance; (void)len;
@@ -207,15 +210,41 @@ int main(void) {
     else { tud_set_config_mode(false); }
     
     tud_init(BOARD_TUD_RHPORT);
-    if (!tud_in_config_mode()) { multicore_launch_core1(core1_main); }
+    multicore_launch_core1(core1_main);
     last_math_tick_us = time_us_32();
     
     while (true) {
         tud_task(); 
-        if (tud_in_config_mode()) {
-            process_web_serial_commands(); 
+ if (tud_in_config_mode()) {
+            process_web_serial_commands();
+
+            uint32_t now_us = time_us_32();
+            if (now_us - last_math_tick_us >= 4000) {
+                last_math_tick_us += 4000;
+                uint32_t ints = save_and_disable_interrupts();
+                int16_t lx = raw_lx, ly = raw_ly, rx = raw_rx, ry = raw_ry;
+                restore_interrupts(ints);
+                humanizer_process(&humanizer, &lx, &ly, &rx, &ry,
+                                  active_config.circ_error,
+                                  active_config.jitter_mag, active_config.jitter_inner, active_config.jitter_outer,
+                                  active_config.smoothing_rate, active_config.gate_level,
+                                  active_config.tilt_deg, active_config.landing_var,
+                                  active_config.diagonal_feel, active_config.anti_deadzone,
+                                  active_config.passthrough);
+                preview_lx = lx; preview_ly = ly;
+            }
+
+            uint32_t now_ms = to_ms_since_boot(get_absolute_time());
+            if (now_ms - last_preview_ms >= 33) {
+                last_preview_ms = now_ms;
+                char msg[24];
+                int n = snprintf(msg, sizeof(msg), "P:%d,%d\n", preview_lx, preview_ly);
+                if (n > 0) { tud_cdc_write((const uint8_t*)msg, (uint32_t)n); tud_cdc_write_flush(); }
+            }
+
             if (pending_save_and_reboot) {
                 if (to_ms_since_boot(get_absolute_time()) - save_trigger_time > 500) {
+                    multicore_reset_core1();
                     save_settings_to_flash(&active_config);
                     watchdog_hw->scratch[0] = 0;
                     watchdog_reboot(0, 0, 10);
